fed13ee7b1d38f9bf36009132d9cd07a4ffb25c8
AlexisCastillo10 <alexiscastillods@gmail.com>
2024-12-10 16:30:34 -0500

    Panel, Sensor Historico
    Se crea el panel con la funcionalidad de la lectura en tiempo real de los sensores. le panel es fijo y lee datos de un scrip.

--- hubsbrowser24_DigitalTwins/obj/Debug/net8.0/hubsbrowser24.AssemblyInfo.cs
+++ hubsbrowser24_DigitalTwins/obj/Debug/net8.0/hubsbrowser24.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hubsbrowser24")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fab9e3869bd6af0afb979a6575b716536351b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248a5be5dee97fdd8f1799fa5ab18e860d518c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("hubsbrowser24")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hubsbrowser24")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
